cf5f17b1b39840cafc192a887edc1d728c05deb0
anolkurian <38421225+anolkurian@users.noreply.github.com>
2018-04-16 14:13:50 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,13 @@
+
 import java.util.Scanner;
 class helloworld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD ");
+		int sum;
+		int a=5,b=6;
+		sum=a+b;
+		System.out.println(sum);
 	}
 }
