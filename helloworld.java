ea739adb05f0d006efe794829fe1e68a97e3aa34
anolkurian <38421225+anolkurian@users.noreply.github.com>
2018-04-16 14:24:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,9 +5,9 @@ class helloworld
 	public static void main(String[] args)
 	{
 		System.out.println("HELLO WORLD ");
-		int sum;
-		int a=5,b=6;
-		sum=a+b;
-		System.out.println(sum);
+// 		int sum;
+// 		int a=5,b=6;
+// 		sum=a+b;
+// 		System.out.println(sum);
 	}
 }
